332d70094b9b8b23eaf44d5bc9c5862368508db7
Lagaalett <daval.clemence@gmail.com>
2021-04-15 22:33:05 +0200

    ajout d'une animation quand les settings apparaissent et suppression du component SignIn (gestion de l'affichage ou non des champs de formulaire avec un display none)

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,19 +1,33 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import PropTypes from 'prop-types';
-import SignIn from 'src/containers/SignIn';
 
 import './settings.scss';
 
-const Settings = ({ isOpen, toggleSettings }) => {
+const Settings = ({
+  isOpen, toggleSettings, newLogin, newPassword, setNewLogin, setNewPassword,
+}) => {
   const handleClick = () => {
     toggleSettings();
+  };
+  const handleChangeLogin = (event) => {
+    const newValue = event.target.value;
+    setNewLogin(newValue);
+  };
+
+  const handleChangePassword = (event) => {
+    const newValue = event.target.value;
+    setNewPassword(newValue);
   };
 
   return (
     <div className="settings">
-      <button className="settings__button" type="button" onClick={handleClick}> + </button>
-      {isOpen && <SignIn />}
+      <button className={isOpen ? 'settings__button settings__button--open' : 'settings__button'} type="button" onClick={handleClick}> + </button>
+      <form className={isOpen ? 'settings__form settings__form--open' : 'settings__form'}>
+        <input className="settings__form__input" type="email" placeholder="Email" value={newLogin} onChange={handleChangeLogin} />
+        <input className="settings__form__input" type="password" placeholder="Mot de passe" value={newPassword} onChange={handleChangePassword} />
+        <button className="settings__form__submitButton" type="submit"> Envoyer </button>
+      </form>
     </div>
   );
 };
@@ -21,6 +35,10 @@ const Settings = ({ isOpen, toggleSettings }) => {
 Settings.propTypes = {
   toggleSettings: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
+  newLogin: PropTypes.string.isRequired,
+  newPassword: PropTypes.string.isRequired,
+  setNewLogin: PropTypes.func.isRequired,
+  setNewPassword: PropTypes.func.isRequired,
 };
 
 export default Settings;
--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 
 import Settings from 'src/components/Settings';
-import { toggleSettings } from 'src/actions';
+import { toggleSettings, setLogin, setPassword } from 'src/actions';
 
 // connection de props en lecture sur le state
 // ces props seront des tableaux, objets, booléens, numériques, string
 const mapStateToProps = (state, ownProps) => ({
   isOpen: state.open,
+  newLogin: state.login,
+  newPassword: state.password,
 });
 
 // connection de props fonctions qui déclenchent des actions
@@ -14,6 +16,12 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   toggleSettings: () => {
     dispatch(toggleSettings());
+  },
+  setNewLogin: (newValue) => {
+    dispatch(setLogin(newValue));
+  },
+  setNewPassword: (newValue) => {
+    dispatch(setPassword(newValue));
   },
 });
 
